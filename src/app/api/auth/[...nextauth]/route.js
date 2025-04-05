8175fc7f148619b18787db20d12baf0272440cd9
Yao Long <yaolongt@gmail.com>
2025-04-05 12:57:39 +0800

    fix: remove token setting from frontend

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -13,11 +13,15 @@ export const config = {
       },
       async authorize(credentials) {
         try {
-          const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/login`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(credentials),
-          });
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/user/login`,
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              credentials: "include",
+              body: JSON.stringify(credentials),
+            }
+          );
           const data = await res.json();
 
           if (!res.ok) {
@@ -28,13 +32,13 @@ export const config = {
             throw new Error("Token missing in response");
           }
 
-          const tokenExpire = 60 * 60 * 24;
-          cookies().set("token", data.token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: tokenExpire,
-          });
+          // const tokenExpire = 60 * 60 * 24;
+          // cookies().set("token", data.token, {
+          //   httpOnly: true,
+          //   secure: process.env.NODE_ENV === "production",
+          //   sameSite: "strict",
+          //   maxAge: tokenExpire,
+          // });
 
           return { token: data.token, user: { email: credentials.email } };
         } catch (error) {
